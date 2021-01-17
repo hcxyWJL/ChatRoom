987ddb235a406932630c14c9894b7c2c0751a1ac
21 <1052967184@qq.com>
2021-01-17 21:23:57 +0800

    实现了群发和私聊

--- src/cn/edu/hcen/client/ChatThreadWindow.java
+++ src/cn/edu/hcen/client/ChatThreadWindow.java
@@ -1,6 +1,10 @@
 package cn.edu.hcen.client;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.net.*;
 import java.sql.*;
@@ -24,7 +28,7 @@ public class ChatThreadWindow {
     DatagramSocket ds;
     private JTextField tf;
     private static int total;// 在线人数统计
-
+    JButton searchBtn;
     public ChatThreadWindow(String name,DatagramSocket ds) {
         this.ds=ds;
         this.name=name;
@@ -47,6 +51,37 @@ public class ChatThreadWindow {
         JPanel p = new JPanel(new BorderLayout());
         p.add(pl, BorderLayout.WEST);
         p.add(tf);
+        tf.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                        if(e.getKeyCode()==KeyEvent.VK_ENTER)
+                        {
+                            String messageTO=(String)cb.getSelectedItem();
+                            if ("All".equals(messageTO))
+                            {
+                                //群聊
+                                SendALL();
+                                searchBtn.doClick();
+                            }
+                            else {
+                                //私聊
+                                oneTOone();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
         f.getContentPane().add(p, BorderLayout.SOUTH);
         f.getContentPane().add(sp);
         f.setVisible(true);
@@ -55,6 +90,122 @@ public class ChatThreadWindow {
         GetMessageThread getMessageThread=new GetMessageThread(this);
         getMessageThread.start();
         showXXXintochatromm();
+        showXXXinchatromm();
+    }
+    public void oneTOone()
+    {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstm=null;
+        Connection conn=null;
+        try {
+            conn= DriverManager.getConnection(url,username_db,password_db);
+            String sql="SELECT  USERNAME,ip,port FROM users WHERE status='online'and USERNAME=?";
+            pstm=conn.prepareStatement(sql);
+            String messageTO=(String)cb.getSelectedItem();
+            pstm.setString(1,messageTO);
+            ResultSet rs=pstm.executeQuery();
+            while (rs.next())
+            {
+                String username=rs.getString("USERNAME");
+                String ip= rs.getString("IP");
+                int port=rs.getInt("PORT");
+                System.out.println(ip);
+                System.out.println(port);
+                byte [] ipB=new byte[4];
+                String ips[]=ip.split("\\.");
+
+                for (int i=0;i<ips.length;i++)
+                {
+                    ipB[i]=(byte)Integer.parseInt(ips[i]);
+                }
+                if (!username.equals(name))
+                {
+                    String message=tf.getText();
+                    byte []m=message.getBytes();
+                    DatagramPacket dp=new DatagramPacket(m,m.length);
+                    System.out.println("");
+                    dp.setAddress(InetAddress.getByAddress(ipB));
+                    dp.setPort(port);
+                    DatagramSocket ds=new DatagramSocket();
+                    ds.send(dp);//投递
+                }
+            }
+        } catch (SQLException | UnknownHostException | SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void SendALL()
+    {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstm=null;
+        Connection conn=null;
+        try {
+            conn= DriverManager.getConnection(url,username_db,password_db);
+            String sql="SELECT  USERNAME,ip,port FROM users WHERE status='online'";
+            pstm=conn.prepareStatement(sql);
+            ResultSet rs=pstm.executeQuery();
+            while (rs.next())
+            {
+                String username=rs.getString("USERNAME");
+                String ip= rs.getString("IP");
+                int port=rs.getInt("PORT");
+                System.out.println(ip);
+                System.out.println(port);
+                byte [] ipB=new byte[4];
+                String ips[]=ip.split("\\.");
+
+                for (int i=0;i<ips.length;i++)
+                {
+                    ipB[i]=(byte)Integer.parseInt(ips[i]);
+                }
+                if (!username.equals(name))
+                {
+                    String message=tf.getText();
+                    byte []m=message.getBytes();
+                    DatagramPacket dp=new DatagramPacket(m,m.length);
+                    System.out.println("");
+                    dp.setAddress(InetAddress.getByAddress(ipB));
+                    dp.setPort(port);
+                    DatagramSocket ds=new DatagramSocket();
+                    ds.send(dp);//投递
+                }
+            }
+        } catch (SQLException | UnknownHostException | SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showXXXinchatromm()
+    {
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        String username_db = "opts";
+        String password_db = "opts1234";
+        PreparedStatement pstm=null;
+        Connection conn=null;
+        try {
+            conn= DriverManager.getConnection(url,username_db,password_db);
+            String sql="SELECT  USERNAME,ip,port FROM users WHERE status='online' and  USERNAME!=?";
+            pstm=conn.prepareStatement(sql);
+            pstm.setString(1,name);
+            ResultSet rs=pstm.executeQuery();
+            while (rs.next())
+            {
+                String username=rs.getString("USERNAME");
+                String message=username+"正在聊天室";
+                ta.append(message);
+                cb.addItem(username);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     public void showXXXintochatromm()
     {
@@ -87,6 +238,7 @@ public class ChatThreadWindow {
                     String message=name+"进入了聊天室";
                     byte []m=message.getBytes();
                     DatagramPacket dp=new DatagramPacket(m,m.length);
+                    System.out.println("");
                     dp.setAddress(InetAddress.getByAddress(ipB));
                     dp.setPort(port);
                     DatagramSocket ds=new DatagramSocket();
